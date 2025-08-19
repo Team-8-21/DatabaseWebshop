737c9c2588bde1a94ac426d295ff87291679b6ac
Megan <meganebsen@gmail.com>
2025-08-19 14:45:44 +0200

    Order og Customer

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -8,5 +8,15 @@ namespace DatabaseWebshop.Model
 {
     public class Customer
     {
+        public int CustomerID { get; set; } // Primærnøgle
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+        public int ZipCode { get; set; }
+
+        public List<Order> Orders { get; set; } = new(); // Navigation til ordrer
+
+       
     }
 }
---
+++ Model/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DatabaseWebshop.Model
+{
+    public class Order : Customer
+    {
+        public int OrderId { get; set; }
+        
+        public DateTime OrderDate { get; set; }
+        public double Total { get; set; }
+
+        public int CustomerID { get; set; } // Fremmednøgle
+        public Customer Customer { get; set; } // Navigation til Customer
+
+
+    }
+}
